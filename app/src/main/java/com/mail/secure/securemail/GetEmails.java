4cc0023784804d04fac17b8bd3774dc9f911e458
abdula2i2 <32874191+abdula2i2@users.noreply.github.com>
2017-11-18 18:35:51 +0300

    Send and res

---
+++ app/src/main/java/com/mail/secure/securemail/GetEmails.java
@@ -0,0 +1,133 @@
+package com.mail.secure.securemail;
+
+/**
+ * Created by zeez on 10/25/2017.
+ */
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.sun.mail.imap.IMAPFolder;
+import java.io.IOException;
+import java.util.Properties;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Store;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+public class GetEmails extends AsyncTask<Void,Void,Void> {
+   // private static final String email_id = "msaproj@gmail.com";
+   // private static final String password = ".1234567";
+    private ProgressDialog progressDialog;
+    private Context context;
+    private Realm realm;
+    private String folder;//هذا عشان تحدد القسم الي بتاخذ منه الرسايل
+    private boolean success;// عشان اذا نجح يقفل الاكتفتي واذا فشل يمسح البيانات حق الدخول
+
+    public GetEmails(Context context , String folder) {
+        this.context = context;
+        this.folder = folder;
+    }
+    public void setFolder  (String folder){this.folder=folder;}
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        //هذا عشان يطلع للمستخدم وقت محاوله تسجيل الدخول
+      progressDialog = ProgressDialog.show(context,"signing in  ","Please wait...",false,false);
+
+    }
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        //هنا عشان يوقف شاشة التحميل ويقرر يوديه للميل بوكس او يمسح البيانات الخطاء الي سجلها
+        progressDialog.dismiss();
+        if (this.success){
+            ((Activity)context).finish();// عشان يقفل الاكتفتي ويروح لين الميل بوكس
+        }
+        else {// تمسح البيانات الخطاء الي دخلها المستخدم وتطلع له رساله
+            realm = Realm.getDefaultInstance();
+            RealmResults<User> result = realm.where(User.class).findAll();
+            User user = result.first();
+
+            realm.beginTransaction();
+            user.deleteFromRealm();
+            realm.commitTransaction();
+            Toast.makeText(context, "Login Failed", Toast.LENGTH_LONG).show();        //Showing a success message
+        }
+
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+
+        realm = Realm.getDefaultInstance();
+        User user = realm.where(User.class).equalTo("status", "active").findFirst();
+        final String email_id = user.getEmail(); // تاخذ الاسم واليوزر وتحفظهم في مخازن فاينل للاتصال
+        final String password = user.getPassword();
+
+        //تحديد خصائص الاتصال
+        Properties properties = new Properties();
+        properties.put("mail.store.protocol", "imap"); //تحديد البروتوكول
+        properties.put("mail.imap.host", "192.168.56.1");//تحديد الاميل سيرفر
+        properties.put("mail.imap.port", "143");//تحديد البورت
+        //properties.setProperty( "mail.imaps.socketFactory.class", "com.mail.secure.securemail.MySSLSocketFactory" );// هذا عشان يحل مشكلة الشهادة
+
+        try
+
+        {   // محاولة الاتصال بالاميل سيرفر
+            Session session = Session.getDefaultInstance(properties, null);
+            Store store = session.getStore("imap");
+            store.connect(email_id, password);
+            IMAPFolder folder = (IMAPFolder) store.getFolder(this.folder);//هنا تحدد القسم الي بتاخذ منه الرسايل مثلا الانبوكس
+            folder.open(Folder.READ_ONLY);
+
+
+            int messageCount = folder.getMessageCount(); // تطلع عدد الرسائل
+
+            Emails email = new Emails();
+            realm.beginTransaction();
+            //تاخذ الاميلات وتخزنها في قاعدة البيانات
+            for (int i = 1; i <= messageCount; i++) {
+
+                Message m = folder.getMessage(i);
+//                BodyPart bp = ((Multipart) m.getContent()).getBodyPart(0); هذا مع اميلات قوقل يزبط
+                email.setMessage(m.getContent().toString());
+                email.setSubject(m.getSubject().toString());
+                email.setSender(m.getFrom()[0].toString());
+                user.getInbox().add(email);
+
+            }
+            realm.copyToRealmOrUpdate(user);// بعد ما تاخذ كل الاميلات توديها في قاعدة البيانات
+            realm.commitTransaction();
+            realm.close();// تقفل قاعدة البيانات
+            folder.close(true);// تقفل اتصالك
+            store.close();
+            this.success = true; // معناها نجح وراح يطلع رسالة انه دخل
+
+        } catch (MessagingException | IOException e) {
+            e.printStackTrace();this.success = false; // اذا جاء للكاتش معناها انه ما دخل فا بتطلع رسالة انه فشل في الدخول
+             }
+
+            return null;
+
+           }
+
+        }
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/mail/secure/securemail/SetEmails.java
@@ -0,0 +1,108 @@
+package com.mail.secure.securemail;
+
+/**
+ * Created by zeez on 10/25/2017.
+ */
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import io.realm.Realm;
+
+public class SetEmails extends AsyncTask<Void,Void,Void> {
+//    private static final String email_id = "msaproj@gmail.com";
+//    private static final String password = ".1234567";
+    private String content, to, subject;
+    private Context context;
+    private Realm realm;
+    private boolean success;
+
+    public SetEmails(Context context, String to, String subject, String content) {
+        this.content = content;
+        this.subject = subject;
+        this.to = to;
+        this.context = context;
+        this.success = true;
+    }
+
+
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        //اظهار رسالة على حسب اذا نجح او اذا وصل للكاتش
+        if (success){
+        Toast.makeText(context, "Message sent", Toast.LENGTH_LONG).show();}
+        else Toast.makeText(context, "Message Failed", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+
+        realm = Realm.getDefaultInstance();
+        // تاخذ من قاعده البيانات الايوزر والباسورد وتحطهم في مخازن فاينل عشان الاتصال
+        User user = realm.where(User.class).equalTo("status", "active").findFirst();
+        final String email_id = user.getEmail();
+        final String password = user.getPassword();
+
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");//كلام جرايد
+            props.put("mail.smtp.starttls.enable", "true");//كلام جرايد مدري وش
+            props.put("mail.smtp.ssl.trust", "192.168.56.1");//هذي ما اتوقع منها فايده الا في النت بينز
+            props.put("mail.smtp.host", "192.168.56.1");//تحدد السيرفر
+      //  props.setProperty( "mail.smtp.socketFactory.class", "com.mail.secure.securemail.MySSLSocketFactory" ); // تحل مشكله الشهاده مع الجيميل
+            props.put("mail.smtp.port", "25");// تحدد البورت
+
+            // يحاول يسوي اتصال باليوزر والباسورد
+            Session session = Session.getInstance(props,
+                    new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(email_id, password);
+                        }
+                    });
+
+            try {
+                // اذا قبل الاتصال تسوي رساله بالاتصال اذا مشى
+                Message message = new MimeMessage(session);
+
+                // تحدد الاميل الي مرسله منه الرسالة
+                message.setFrom(new InternetAddress(email_id));
+
+                // Set To: header field of the header. مدري كلام كويس
+                message.setRecipients(Message.RecipientType.TO,
+                        InternetAddress.parse(to));
+
+                message.setSubject(subject);// العنوان
+                message.setText(content);// المحتوى الي هو الرسالة
+                Transport.send(message);// بدأ الارسال
+
+            } catch (MessagingException e) {this.success = false; // اذا صارت مشكلة يجي هنا ويغير القيمه عشان فوق تطلع رساله انه لم يتم الارسال
+                     e.printStackTrace();
+            }
+
+            return null;
+        }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
